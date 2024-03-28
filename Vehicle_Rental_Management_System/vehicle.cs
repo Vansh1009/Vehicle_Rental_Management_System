819f02c1513331ee4ccbb0f3df8dd0c0cdff0ac7
Vansh1009 <vansh.palkhiwala10@gmail.com>
2024-03-27 20:39:19 -0400

    Vehicle class

---
+++ Vehicle_Rental_Management_System/vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Vehicle
+{
+    // Properties
+    public string Model { get; private set; }
+    public string Manufacturer { get; private set; }
+    public int Year { get; private set; }
+    public double RentalPrice { get; private set; }
+
+    // Constructor
+    public Vehicle(string model, string manufacturer, int year, double rentalPrice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+
+    // Method to display vehicle details
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}");
+        Console.WriteLine($"Manufacturer: {Manufacturer}");
+        Console.WriteLine($"Year: {Year}");
+        Console.WriteLine($"Rental Price: {RentalPrice:C}");
+    }
+}

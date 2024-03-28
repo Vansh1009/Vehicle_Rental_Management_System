4df12ceabf2c323cf96ad3b6ce8c51330d7a1669
Vansh1009 <vansh.palkhiwala10@gmail.com>
2024-03-27 20:46:54 -0400

    Motorcycle class

---
+++ Vehicle_Rental_Management_System/Motorcycle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Motorcycle : Vehicle
+{
+    // Additional properties
+    public int EngineCapacity { get; private set; }
+    public string FuelType { get; private set; }
+    public bool HasFairing { get; private set; }
+
+    // Constructor
+    public Motorcycle(string model, string manufacturer, int year, double rentalPrice,
+                      int engineCapacity, string fuelType, bool hasFairing)
+        : base(model, manufacturer, year, rentalPrice)
+    {
+        EngineCapacity = engineCapacity;
+        FuelType = fuelType;
+        HasFairing = hasFairing;
+    }
+
+    // Override method to display motorcycle details
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Engine Capacity: {EngineCapacity} cc");
+        Console.WriteLine($"Fuel Type: {FuelType}");
+        Console.WriteLine($"Has Fairing: {(HasFairing ? "Yes" : "No")}");
+    }
+}
+using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
